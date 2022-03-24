4506805b567e7b7391243587065f808d42866315
Ardanudn <63992156+Ardanudn@users.noreply.github.com>
2022-03-24 08:09:24 +0700

    Praktikum Minggu 3

---
+++ app/src/main/java/com/example/ardanudhuhrinugroho/ui/mhs.java
@@ -0,0 +1,20 @@
+package com.example.ardanudhuhrinugroho.ui;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.ardanudhuhrinugroho.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class mhs extends Fragment {
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View root=inflater.inflate(R.layout.fragment_mhs,container,false);
+        return root;
+    }
+}
